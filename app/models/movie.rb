bc5738b16ac26f7cb5becdfc8930ef27fc81c893
Corey <burkleycorey@yahoo.com>
2015-09-10 17:05:24 -0500

    no fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ActiveRecord::Base
     
   validates :moviename, presence: true, length: { minimum: 3, maximum: 50 }
   validates :genre, presence: true, length: { minimum: 5, maximum: 50 }
-  
+
 end
