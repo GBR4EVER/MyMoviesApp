04b5a73950afa0be5a960fa14573c2528ab04f89
Corey <burkleycorey@yahoo.com>
2015-09-10 16:42:51 -0500

    Made validation tests for Review Model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
   
-  
+  validates :author, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :summary, presence: true, length: { minimum: 10, maximum: 200 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 500 }
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -7,12 +7,52 @@ class ReviewTest < ActiveSupport::TestCase
               description: "A man walks into a terrible scenario.")
   end
   
-  test "review shouls be valid" do
+  test "review should be valid" do
     assert @review.valid?
   end
   
   test "author should be present" do
     @review.author = " "
+    assert_not @review.valid?
+  end
+  
+  test "author length should not be too short" do
+    @review.author = "a" * 101
+    assert_not @review.valid?
+  end
+  
+  test "author length should not be too long" do
+    @review.author = "a" * 4
+    assert_not @review.valid?
+  end
+  
+  test "summary must be present" do
+    @review.summary = " "
+    assert_not @review.valid?
+  end
+  
+  test "summary length should not be too long" do
+    @review.summary = "b" * 201
+    assert_not @review.valid?
+  end
+  
+  test "summary length should not be too short" do
+    @review.summary = "b" * 9
+    assert_not @review.valid?
+  end
+  
+  test "description must be present" do
+    @review.description = " "
+    assert_not @review.valid?
+  end
+  
+  test "description length should not be too long" do
+    @review.description = "c" * 501
+    assert_not @review.valid?
+  end
+  
+  test "description length should not be too short" do
+    @review.description = "c" * 19
     assert_not @review.valid?
   end
   
