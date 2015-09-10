8ab0d3a23c4c9c6cf1c53fcb9d5d21cf161307f6
Corey <burkleycorey@yahoo.com>
2015-09-10 16:51:47 -0500

    Added validations to Movie Model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
+    
+  validates :moviename, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :genre, presence: true, length: { minimum: 5, maximum: 50 }
   
 end
