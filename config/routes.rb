94de25a4965c85b218ea2e71b6db4c5ae652c91c
Corey <burkleycorey@yahoo.com>
2015-09-14 18:01:48 -0500

    Added reviews resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   
   get '/home', to: 'pages#home'
   
+  # REVIEW ROUTES
+  # get '/reviews', to: 'reviews#index'
+  # get '/reviews/new', to: 'reviews#new', as: 'new_review'
+  # post '/reviews', to: 'reviews#create'
+  # get '/reciews/:id/edit', to: 'reviews#edit', as: 'edit_review'
+  # patch '/reviews/:id', to: 'reviews#update'
+  # get '/reviews/:id', to: 'reviews#show', as: 'review'
+  # delete '/reviews/:id', to: 'reviews#destroy'
+  
+  resources :reviews 
 end
