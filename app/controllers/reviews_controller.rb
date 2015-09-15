ea386a8413123dbd5b2bac74cb3f7bc4b514b22f
Corey <burkleycorey@yahoo.com>
2015-09-15 18:54:33 -0500

    Added forms and new and create actions within the Reviews controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,5 +8,27 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id]) 
   end
   
+  def new
+    @review = Review.new
+  end
   
+  def create
+    @review = Review.new(review_params)
+    @review.movie = Movie.find(1)
+    
+    if @review.save
+      flash[:success] = "Your Review was created successfully!"
+      redirect_to reviews_path
+    else
+      render :new
+    end
+  end
+  
+  
+  private
+  
+    def review_params
+      params.require(:review).permit(:author, :email, :description, :summary)
+    end
+    
 end
